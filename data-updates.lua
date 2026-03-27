64c286deea4d9af178c22364ae1faa3bf3db7686
Talandar <talandar66@protonmail.com>
2026-03-27 14:45:42 +0100

    waterway from start of the game

--- data-updates.lua
+++ data-updates.lua
@@ -2,3 +2,5 @@ if mods["pirateship"] then
 	table.insert(data.raw.technology["steel-axe"].effects, { type = "unlock-recipe", recipe = "pirateship" })
 end
 table.insert(data.raw.technology["steel-axe"].effects, { type = "unlock-recipe", recipe = "port" })
+
+data.raw["shortcut"]["give-waterway"].technology_to_unlock = "steel-axe"
